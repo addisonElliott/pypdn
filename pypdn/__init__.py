493b7e0531757cc84620bd5b05f628101c28d457
Addison Elliott <addison.elliott@gmail.com>
2019-06-20 20:14:20 -0500

    Fix typo

--- pypdn/__init__.py
+++ pypdn/__init__.py
@@ -1,4 +1,4 @@
-from polarTransform._version import __version__
+from pypdn._version import __version__
 from pypdn.reader import read, BlendType, Layer, LayeredImage
 from pypdn.util import JSONEncoder
 from pypdn.nrbf import NRBF
